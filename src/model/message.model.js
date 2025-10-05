fdb87886137b17e11ced23ff4a18748f034e52e1
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-06 03:22:07 +0600

    update

---
+++ src/model/message.model.js
@@ -0,0 +1,31 @@
+const mongoose= require('mongoose')
+
+const messageScehma= new mongoose.Schema({
+    name:{
+        type:String,
+        required: [ true ,'Name is  required']
+    },
+    email:{
+        type: String,
+        required: [ true ,'email is  required'],
+        trim: true
+    },
+    subject:{
+        type: String,
+        required: [ true ,'Subject is  required'],
+        trim: true
+    },
+    message:{
+        
+        type: String,
+        required: [ true ,'Subject is  required']
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+
+const Message= mongoose.model('messages', messageScehma)
