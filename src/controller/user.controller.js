92e35a002354ccfb7c3b4299656df7a9028ce7cf
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-12 19:16:18 +0600

    update

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -88,7 +88,7 @@ const loginUser = async (req, res) => {
 
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(404).send({ success: false, message: "User not found" });
+      return res.status(404).send({ success: false, message: "User not found with this emai. Please signup" });
     }
 
     const passwordMatch = await bcrypt.compare(password, user.password);
