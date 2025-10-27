38078e83d7f00e0a0ca03ffdbcf5a90543cdac5a
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-27 18:39:18 +0600

    update

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'https://ccirl.netlify.app',
+    origin: ['https://ccirl.netlify.app','http://localhost:3000'],
     credentials: true
 }))
 
